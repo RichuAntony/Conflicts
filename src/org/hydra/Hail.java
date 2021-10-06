27bdeeef7d67621b96f4b8d85b7b7ece35ff0b35
RichuAntony <richardtony006@gmail.com>
2021-10-06 19:50:31 +0530

    new project

---
+++ src/org/hydra/Hail.java
@@ -0,0 +1,27 @@
+package org.hydra;
+
+import javax.swing.Spring;
+
+public class Hail {
+	private void captian() {
+		System.out.println("captian");
+		
+	}
+	public void ironMan() {
+		System.out.println("ironMan");
+		
+		
+	}
+	private void thor() {
+		System.out.println("thor");
+		
+	}
+	public static void main(String[]args) {
+		Hail a=new Hail();
+		a.captian();
+		a.ironMan();
+		a.thor();
+		
+	}
+
+}
